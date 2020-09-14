cbbaf6ef533bc6994ccc2cc0535d74ba7c198d8a
Swati N Murthy <swati.snm20@gmail.com>
2020-09-14 09:54:49 +0530

    Changes in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -7,10 +7,11 @@ const initialState = {};
 
 const middleware = [thunk];
 
-//const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(
+   rootReducer,
+   initialState,
+   composeEnhancers(applyMiddleware(...middleware))
+);
 
 export default store;
